42f32fff1484b1e0fcadc1d80f3f198308e64d6a
Your Name <you@example.com>
2023-12-24 18:45:08 +0100

    Initial commit

---
+++ src/main/java/com/todocine/TodocineApplication.java
@@ -0,0 +1,13 @@
+package com.todocine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodocineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodocineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todocine/TodocineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodocineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
