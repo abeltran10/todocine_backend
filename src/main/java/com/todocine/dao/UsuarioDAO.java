4595fd91461b72008d03be511a285e17579533de
Your Name <you@example.com>
2023-12-26 18:52:46 +0100

    removed imports not needed

--- src/main/java/com/todocine/dao/UsuarioDAO.java
+++ src/main/java/com/todocine/dao/UsuarioDAO.java
@@ -1,7 +1,6 @@
 package com.todocine.dao;
 
 import com.todocine.dto.UsuarioDTO;
-import com.todocine.model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UsuarioDAO extends MongoRepository<UsuarioDTO, String> {
--- src/main/java/com/todocine/model/Usuario.java
+++ src/main/java/com/todocine/model/Usuario.java
@@ -3,7 +3,6 @@ package com.todocine.model;
 import com.todocine.dto.UsuarioDTO;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
