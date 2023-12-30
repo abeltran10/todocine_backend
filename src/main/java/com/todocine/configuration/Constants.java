9b441aee558aa9ddb8e6f32af71c94a19cf2ac3d
Your Name <you@example.com>
2023-12-30 21:43:57 +0100

    properties files

---
+++ src/main/java/com/todocine/configuration/Constants.java
@@ -0,0 +1,14 @@
+package com.todocine.configuration;
+
+
+public class Constants {
+
+    public static final long TOKEN_EXPIRATION_TIME = 60 * 60 * 1000;
+
+    public static final String SUPER_SECRET_KEY = "d3sbordats10";
+
+    public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
+
+    public static final String TOKEN_BEARER_PREFIX = "Bearer";
+
+}
