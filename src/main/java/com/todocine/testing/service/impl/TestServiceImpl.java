36288ff590bdca61a187306aa7e597d1cec8a6b9
Your Name <you@example.com>
2024-01-16 19:53:51 +0100

    TestServiceImpl @Service

--- src/main/java/com/todocine/testing/service/impl/TestServiceImpl.java
+++ src/main/java/com/todocine/testing/service/impl/TestServiceImpl.java
@@ -3,7 +3,9 @@ package com.todocine.testing.service.impl;
 import com.todocine.dao.UsuarioDAO;
 import com.todocine.testing.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TestServiceImpl implements TestService {
 
     @Autowired
