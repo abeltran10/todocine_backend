fc04a151381bb1ec17380382bfa4dd7d24ca5e24
Your Name <you@example.com>
2024-06-22 18:14:35 +0200

    added CheckPremioControllerTest

---
+++ src/test/java/com/todocine/CheckPremioControllerTest.java
@@ -0,0 +1,44 @@
+package com.todocine;
+
+import com.todocine.controller.PremioController;
+import com.todocine.dto.PremioDTO;
+import com.todocine.exceptions.NotFoudException;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CheckPremioControllerTest {
+    public static Logger LOG = LoggerFactory.getLogger(CheckPremioControllerTest.class);
+
+    @Autowired
+    private PremioController premioController;
+
+    @Test
+    void getPremioById() {
+        ResponseEntity<PremioDTO> responseEntity = premioController.getPremioById("6676f0933659e96e81401f3b");
+
+        assertEquals("La sociedad de la nieve", responseEntity.getBody().getCategorias().get(0).getMovie().getTitle());
+
+    }
+
+    @Test
+    void getPremioByIdException() {
+        try {
+            premioController.getPremioById("0");
+        } catch (NotFoudException ex) {
+            assertEquals(HttpStatus.NOT_FOUND, ex.getStatusCode());
+        }
+
+
+
+    }
+
+}
