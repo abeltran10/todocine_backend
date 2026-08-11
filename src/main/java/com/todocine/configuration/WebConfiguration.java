66c126b319a7957d645ec6c6cda58160ae733ef9
abeltran10 <you@example.com>
2026-08-11 18:22:34 +0200

    refactored

--- src/main/java/com/todocine/configuration/WebConfiguration.java
+++ src/main/java/com/todocine/configuration/WebConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
